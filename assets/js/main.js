b6539677d2dc91243cad46851ad6a556e1036515
Shantanu Debnath <sdnath017@gmail.com>
2017-07-29 19:22:06 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+
+        $(".embed-responsive iframe").addClass("embed-responsive-item");
+        $(".carousel-inner .item:first-child").addClass("active");
+        
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('.team-carousel').owlCarousel({
+                items:3,
+                margin:30,
+                loop:true,
+                nav:true,
+                navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+            });
+
+    	$('.product-carousel').owlCarousel({
+				items:4,
+				margin:30,
+			    loop:true,
+			    nav:true,
+			    navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+			});
+
+
+     
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
